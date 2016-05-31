340854641bf573113c5ec271b6618465be0ad2ce
Ben Summers <ben.summers@haplo-services.com>
2016-05-31 14:47:20 +0100

    Add license header

--- hres_schema/js/hres_navigation_home_page.js
+++ hres_schema/js/hres_navigation_home_page.js
@@ -1,3 +1,9 @@
+/* Haplo Research Manager                             http://haplo.org
+ * (c) Haplo Services Ltd 2006 - 2016    http://www.haplo-services.com
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.         */
+
 
 // "My stuff" navigation shown on home page if user has a profile object.
 // Plugins can add links below the "My record" by:
