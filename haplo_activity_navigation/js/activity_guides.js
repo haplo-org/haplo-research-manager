f13652e41471b409c107633332e6e4037ee279b9
Ben Summers <ben.summers@haplo-services.com>
2016-09-13 19:16:39 +0100

    Activity navigation: Guides link in app navigation

--- haplo_activity_navigation/js/activity_guides.js
+++ haplo_activity_navigation/js/activity_guides.js
@@ -28,11 +28,8 @@ if(USE_BUILT_IN_GUIDES) {
 
     // ----------------------------------------------------------------------
 
-    var SCREEN_LINK_OPTIONS = {authenticationSignature:true};
-    var PRINT_LINK_OPTIONS =  {authenticationSignature:true, forceDownload:true};
-
-    P.implementService("haplo_activity_navigation:overview", function(activity, add) {
-        add(100, P.template("guides/overview-guides").deferredRender({
+    var deferredRenderGuidesForActivity = function(activity) {
+        return P.template("guides/overview-guides").deferredRender({
             activity: activity,
             sections: _.map(guidesForActivity(activity.name), function(section) {
                 var document = section.document;
@@ -55,7 +52,16 @@ if(USE_BUILT_IN_GUIDES) {
                 };
             }),
             canEdit: O.currentUser.allowed(activity.editAction)
-        }));
+        });
+    };
+
+    // ----------------------------------------------------------------------
+
+    var SCREEN_LINK_OPTIONS = {authenticationSignature:true};
+    var PRINT_LINK_OPTIONS =  {authenticationSignature:true, forceDownload:true};
+
+    P.implementService("haplo_activity_navigation:overview", function(activity, add) {
+        add(100, deferredRenderGuidesForActivity(activity));
     });
 
     // ----------------------------------------------------------------------
@@ -102,4 +108,31 @@ if(USE_BUILT_IN_GUIDES) {
             form: form
         }, "guides/edit-section");
     });
+
+    // --------------------------------------------------------------------------
+
+    P.respond("GET", "/do/activity/guides/all", [
+    ], function(E) {
+        var activityGuides = _.map(P.getActivities(), function(activity) {
+            return {
+                title: activity.title,
+                deferred: deferredRenderGuidesForActivity(activity)
+            };
+        });
+        if(activityGuides.length === 1) {
+            // If there's only one activity in this application, the title
+            // on the guides page would just add visual noise. Remove it.
+            delete activityGuides[0].title;
+        }
+        E.render({activityGuides:activityGuides}, "guides/all");
+    });
+
+    P.hook('hNavigationPosition', function(response, name) {
+        if(name === "haplo:activity-navigation:guides") {
+            var navigation = response.navigation;
+            navigation.separator();
+            navigation.link("/do/activity/guides/all", "Guides");
+        }
+    });
+
 }
