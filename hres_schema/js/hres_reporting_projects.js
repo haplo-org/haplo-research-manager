d775261d26d02366120f45546a993f18d0664df8
Muntasir Syed <mmamunsyed@gmail.com>
2017-02-17 12:25:43 +0000

    HRES Schema: separate out projects dashboards into separate plugin, specify permissions

--- hres_schema/js/hres_reporting_projects.js
+++ hres_schema/js/hres_reporting_projects.js
@@ -1,10 +1,4 @@
 
-P.CanViewResearcherProjectsDashboard = O.action("hres:action:view-researcher-projects-dashboard").
-    title("View Researcher Projects Dashboard").
-    allow("group", Group.Everyone);
-
-// --------------------------------------------------------------------------
-
 P.implementService("std:reporting:discover_collections", function(discover) {
     discover("projects", "Projects");
 });
@@ -23,64 +17,3 @@ P.implementService("std:reporting:collection:projects:get_facts_for_object",
     function(object, row) {
         row.principalInvestigator = object.first(A.Researcher, Q.PrincipalInvestigator);
     });
-
-// --------------------------------------------------------------------------
-
-P.implementService("std:action_panel:researcher", function(display, builder) {
-    if(O.currentUser.allowed(P.CanViewResearcherProjectsDashboard)) {
-        builder.panel(2000).link("default",
-            "/do/hres-navigation/researcher-projects/"+display.object.ref, "Projects");
-    }
-});
-
-P.implementService("std:action_panel:home_page_my_links", function(display, builder) {
-    var roles = O.service("haplo:permissions:user_roles", O.currentUser);
-    if(roles.hasAnyRole("Principal Investigator")) {
-        builder.link(400, "/do/hres-navigation/researcher-projects/"+O.currentUser.ref,
-            "My Projects");
-    }
-});
-
-// --------------------------------------------------------------------------
-
-P.respond("GET,POST", "/do/hres-navigation/researcher-projects", [
-    {pathElement:0, as:"object"}
-], function(E, researcher) {
-    P.CanViewResearcherProjectsDashboard.enforce();
-    var deferredDashboard = P.reporting.dashboard(E, {
-        name: "researcher_projects",
-        kind: "list",
-        collection: "projects",
-        title: "Projects: "+researcher.title
-    }).
-        filter(function(sq) {
-            sq.where("principalInvestigator", "=", researcher.ref);
-        }).
-        columns(100, [
-            {fact:"ref", heading:"Project", link:true}
-        ]).
-        deferredRender();
-    E.render({
-        pageTitle: "Projects: "+researcher.title,
-        backLink: researcher.url(),
-        researcher: researcher,
-        dashboard: deferredDashboard,
-        canCreateProject: !!(O.currentUser.canCreateObjectOfType(T.Project))
-    });
-});
-
-// --------------------------------------------------------------------------
-
-P.respond("GET", "/do/hres-navigation/new-project", [
-    {pathElement:0, as:"object"}
-], function(E, researcher) {
-    var templateObj = O.object();
-    templateObj.appendType(T.Project);
-    templateObj.append(researcher.ref,
-        A.Researcher, Q.PrincipalInvestigator);
-    E.render({
-        pageTitle: "Add new Project",
-        backLink: "/do/hres-navigation/researcher-projects/"+researcher.ref,
-        templateObject: templateObj
-    }, "std:new_object_editor");
-});
---
+++ hres_schema_projects/js/hres_schema_projects.js
@@ -0,0 +1,68 @@
+
+P.CanViewResearcherProjectsDashboard = O.action("hres:action:view-researcher-projects-dashboard").
+    title("View Researcher Projects Dashboard").
+    allow("role", "Head").
+    allow("role", "Research Director").
+    allow("role", "Research Administrator").
+    allow("role", "Principal Investigator");
+
+// --------------------------------------------------------------------------
+
+P.implementService("std:action_panel:researcher", function(display, builder) {
+    if(O.currentUser.allowed(P.CanViewResearcherProjectsDashboard)) {
+        builder.panel(2000).link("default",
+            "/do/hres-navigation/researcher-projects/"+display.object.ref, "Projects");
+    }
+});
+
+P.implementService("std:action_panel:home_page_my_links", function(display, builder) {
+    var roles = O.service("haplo:permissions:user_roles", O.currentUser);
+    if(roles.hasAnyRole("Principal Investigator")) {
+        builder.link(400, "/do/hres-navigation/researcher-projects/"+O.currentUser.ref,
+            "My Projects");
+    }
+});
+
+// --------------------------------------------------------------------------
+
+P.respond("GET,POST", "/do/hres-navigation/researcher-projects", [
+    {pathElement:0, as:"object"}
+], function(E, researcher) {
+    P.CanViewResearcherProjectsDashboard.enforce();
+    var deferredDashboard = P.reporting.dashboard(E, {
+        name: "researcher_projects",
+        kind: "list",
+        collection: "projects",
+        title: "Projects: "+researcher.title
+    }).
+        filter(function(sq) {
+            sq.where("principalInvestigator", "=", researcher.ref);
+        }).
+        columns(100, [
+            {fact:"ref", heading:"Project", link:true}
+        ]).
+        deferredRender();
+    E.render({
+        pageTitle: "Projects: "+researcher.title,
+        backLink: researcher.url(),
+        researcher: researcher,
+        dashboard: deferredDashboard,
+        canCreateProject: !!(O.currentUser.canCreateObjectOfType(T.Project))
+    });
+});
+
+// --------------------------------------------------------------------------
+
+P.respond("GET", "/do/hres-navigation/new-project", [
+    {pathElement:0, as:"object"}
+], function(E, researcher) {
+    var templateObj = O.object();
+    templateObj.appendType(T.Project);
+    templateObj.append(researcher.ref,
+        A.Researcher, Q.PrincipalInvestigator);
+    E.render({
+        pageTitle: "Add new Project",
+        backLink: "/do/hres-navigation/researcher-projects/"+researcher.ref,
+        templateObject: templateObj
+    }, "std:new_object_editor");
+});
