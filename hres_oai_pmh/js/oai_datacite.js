d94565d59096ff8839189965e06cad1009b338bc
Tom Renner <tom.renner@haplo-services.com>
2017-04-28 17:36:35 +0100

    Datacite OAI-PMH handles citation and non-ref attribute values correctly.

--- hres_oai_pmh/js/oai_datacite.js
+++ hres_oai_pmh/js/oai_datacite.js
@@ -14,68 +14,85 @@ var MANDATORY = [
                 name: "creator",
                 relation: A.AuthorsCitation,
                 value: [
-                    {
-                        name: "creatorName",
-                        value: A.Title
-                    },
-                    {
-                        name: "givenName",
-                        value: function(o) { return o.firstTitle().toFields().first; }
-                    },
-                    {
-                        name: "familyName",
-                        value: function(o) { return o.firstTitle().toFields().last; }
-                    },
+                    {name: "creatorName", value: A.Title },
+                    {name: "givenName", value: function(o) { return o.firstTitle().toFields().first; }},
+                    {name: "familyName", value: function(o) { return o.firstTitle().toFields().last; }},
                     {
                         name: "nameIdentifier",
                         value: A.ORCID,
-                        _attr: {
-                            "nameIdentifierScheme": "ORCID"
-                        }
-                    },
-                    {
-                        name: "affiliation",
-                        // TODO: Do properly with standalone entities
-                        value: "University of Jisc"
+                        _attr: {"nameIdentifierScheme": "ORCID"}
                     }
                 ]
             }
         ]
+    },
+    {
+        name:"titles",
+        value: [{ name:"title", value: A.Title }]
+    },
+    {
+        name:"publisher",
+        value: A.Publisher
+    },
+    {
+        name:"publicationYear",
+        value: A.Date
     }
 ];
 
-var build = function(spec, obj) {
+var build = function(spec, context) {
     var i  = {};
 
-    if(typeof(spec.value) === "string") {
-        i[spec.name] = spec.value;
-    } else if(typeof(spec.value) === "object") {
-        i[spec.name] = [];
-        _.each(spec.value, function(s) {
-            var context;
-            if("relation" in s) {
-                // TODO: Handle other relations correctly
-                // TODO: string only citations
-                context = _.map(obj.every(s.relation), function(v) {
-                    var ref = O.service("hres:author_citation:get_ref_maybe", v);
-                    if(ref) { return ref.load(); }
+    // Allow for attributes that could have mixed ref and string values
+    if(O.isRef(context.ref)) {
+        var obj = context;
+        switch(typeof(spec.value)) {
+            case "string":
+                i[spec.name] = spec.value;
+                break;
+            case "object":
+                i[spec.name] = [];
+                // Move context to linked objects
+                _.each(spec.value, function(s) {
+                    if("relation" in s) {
+                        context = _.map(obj.every(s.relation), function(v) {
+                            var ref = O.isRef(v) ?
+                                O.ref(v) :
+                                O.service("hres:author_citation:get_ref_maybe", v);
+                            if(ref) {
+                                return ref.load();
+                            } else {
+                                return v;
+                            }
+                        });
+                    } else {
+                        context = [obj];
+                    }
+                    _.each(context, function(o) {
+                        i[spec.name].push(build(s, o));
+                    });
                 });
-            } else {
-                context = [obj];
-            }
-            _.each(_.compact(context), function(o) {
-                i[spec.name].push(build(s, o));
-            });
-        });
-    } else if(typeof(spec.value) === "function") {
-        i[spec.name] = spec.value(obj);
-    } else {
-        var v = obj.first(spec.value);
-        if(v) {
-            var str = (O.isRef(v) ? v.load().firstTitle() : v).toString();
-            i[spec.name] = str;
+                break;
+            case "function":
+                i[spec.name] = spec.value(obj);
+                break;
+            default:
+                var v = obj.first(spec.value);
+                if(v) {
+                    var str = (O.isRef(v) ? v.load().firstTitle() : v).toString();
+                    i[spec.name] = str;
+                }
+                break;
         }
+    } else {
+        // If context is not a linked object, use value directly
+        // First child value should be the 'title of this context' value
+        var x = {};
+        x[spec.value[0].name] = context.toString();
+        i[spec.name] = [x];
     }
+
+    // Add XML attributes
     if(i[spec.name] && spec._attr) {
         i[spec.name]._attr = spec._attr;
     }
@@ -92,10 +109,10 @@ P.getDataCiteMetadata = function(item) {
 
     return {
         metadata: [
-            {"datacite:resource": [
+            {"resource": [
                 {_attr: {
                     "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-                    "xmlns:datacite": "http://datacite.org/schema/kernel-4",
+                    "xmlns": "http://datacite.org/schema/kernel-4",
                     "xsi:schemaLocation": "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd"
                 }}
             ].concat(metadataItems)}
